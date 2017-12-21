a9e8d414bf3011c65b17473fd7df0185008d03c4
Firaenix <nick.carton@gmail.com>
2017-12-21 23:56:43 +1100

    Added React Markdown

--- src/Sections/80s/Slide1.tsx
+++ src/Sections/80s/Slide1.tsx
@@ -1,7 +1,20 @@
 import * as React from 'react';
+import * as Markdown from 'react-markdown';
 
 export class Slide1 extends React.Component<{}, {}> {
   public render() {
-    return <div className="S80-sS1">The eighties were a rough time for charlotte's anus</div>;
+    return (
+      <div className="S80-sS1">
+        <Markdown className="early-text" source={`# Live demo
+
+Changes are automatically rendered as you type.
+
+* Implements [GitHub Flavored Markdown](https://github.github.com/gfm/)
+* Renders actual, "native" React DOM elements
+* Allows you to escape or skip HTML (try toggling the checkboxes above)
+* If you escape or skip the HTML, no `dangerouslySetInnerHTML` is used! Yay!
+`} />
+      </div>
+    );
   }
 }
